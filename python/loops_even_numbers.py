05620e7af0da711fc73686e4034524e90e58d2ff
WhosJohnGault <Bravesboy1116@gmail.com>
2023-05-08 17:15:28 -0700

    testing new configuragion for visual studo code

---
+++ python/loops_even_numbers.py
@@ -0,0 +1,5 @@
+even_numbers=list(range(2,11,2)) #the inside the paranthesis is ordered s.t. you start are 2, until you reach eleven you increment by 2
+print(even_numbers)
+
+odd_numbers=list(range(1,10,2)) #a similar methodology holds here for our use, we start at 1 and until we reach our upper bound of 10 we increment by 2.
+print(odd_numbers)
---
+++ python/loops_squares.py
@@ -0,0 +1,5 @@
+squares=[]
+for value in range(1,11):  
+    square=value**2               #value**2 denotes taking a number from range and then raising it by a square.
+    squares.append(square)         #when the loop is being ran the value is squared and added to the list.
+print(squares)
